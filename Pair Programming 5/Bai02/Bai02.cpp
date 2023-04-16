d48a3bce1f1490394ac4565e0c1f1bbebbfaa81d
HoangQuoc21 <hoangquoc2106@gmail.com>
2023-04-16 20:17:31 +0700

    Code đôi lần 5

---
+++ Pair Programming 5/Bai02/Bai02.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+struct Node {
+	string code;
+	T weight;
+};
+
+template <class T>
+class PriorityQueue {
+private:
+	Node<T>* a;
+	int n;
+	void ReArrange();
+public:
+	PriorityQueue();
+	~PriorityQueue();
+	void Enqueue(string c, T w);
+	void Dequeue();
+	Node<T> Top();
+	void Print();
+	int Size();
+};
+
+template<class T>
+void PriorityQueue<T>::ReArrange() {
+	for (int i = 0; i < n - 1; i++)
+		for (int j = i + 1; j < n; j++)
+			if (a[i].weight < a[j].weight) {
+				swap(a[i], a[j]);
+			}
+}
+
+template<class T>
+PriorityQueue<T>::PriorityQueue() {
+	a = NULL;
+	n = 0;
+}
+
+template<class T>
+PriorityQueue<T>::~PriorityQueue() {
+	if (a != NULL) {
+		delete[]a;
+		a = NULL;
+	}
+	n = 0;
+}
+
+template<class T>
+void PriorityQueue<T>::Enqueue(string c, T w) {
+	//Tạo mảng b có các phần tử của mảng a và phần tử mới muốn chèn vào
+	Node<T>* b = new Node<T>[n + 1];
+	for (int i = 0; i < n; i++)
+		b[i] = a[i];
+
+	//Thêm phần tử mới vào cuối mảng b
+	b[n].code = c;
+	b[n].weight = w;
+	
+	//Xóa mảng a đi
+	if (a != NULL) {
+		delete[]a;
+	}
+
+	//Gán mảng b cho a
+	a = b;
+	n++;
+
+	//Sắp xếp lại mảng a dựa vào trọng số
+	ReArrange();
+}
+
+template<class T>
+void PriorityQueue<T>::Dequeue() {
+	//Tạo mảng b có các phần tử trừ phẩn tử đầu của mảng a
+	Node<T>* b = new Node<T>[n - 1];
+	for (int i = 0; i < n - 1; i++)
+		b[i] = a[i + 1];
+	
+	//Xóa mảng a đi
+	if (a != NULL) {
+		delete[]a;
+	}
+
+	//Gán mảng b cho a
+	a = b;
+	n--;
+}
+
+template<class T>
+Node<T> PriorityQueue<T>::Top() {
+	return a[0];
+}
+
+template<class T>
+void PriorityQueue<T>::Print() {
+	for (int i = 0; i < n; i++)
+		cout << "(" << a[i].code << "," << a[i].weight << ") ";
+	cout << endl;
+}
+
+template<class T>
+int PriorityQueue<T>::Size() {
+	return n;
+}
+
+int main() {
+	PriorityQueue<int> pq;
+	pq.Enqueue("A", 3);
+	pq.Enqueue("B", 1);
+	pq.Enqueue("C", 7);
+	Node<int> top = pq.Top();
+	cout << "----------------------------------\n";
+	cout << "Top: (" << top.code << "," << top.weight << ")\n";
+	cout << "----------------------------------\n";
+
+	pq.Print();
+	cout << "Queue's size: " << pq.Size() << endl;
+	cout << "----------------------------------\n";
+
+	cout << "Enqueue:\n";
+	pq.Enqueue("D", 5);
+	pq.Print();
+	cout << "Queue's size: " << pq.Size() << endl;
+	cout << "----------------------------------\n";
+
+	cout << "Dequeue:\n";
+	pq.Dequeue();
+	pq.Print();
+	cout << "Queue's size: " << pq.Size() << endl;
+	cout << "----------------------------------\n";
+	return 0;
+}
---
+++ Pair Programming 5/Bai03_fixed/Bai03_fixed.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+template<class T, class U>
+class Point {
+private:
+	T x;
+	U y;
+public:
+	Point();
+	Point(T a, U b);
+	double calDis(Point other);
+	bool operator>(Point other);
+	void print();
+};
+
+template<class T, class U>
+Point<T, U>::Point() {
+	x = T();
+	y = U();
+}
+
+template<class T, class U>
+Point<T, U>::Point(T a, U b) {
+	x = a;
+	y = b;
+}
+
+template<class T, class U>
+double Point<T, U>::calDis(Point other) {
+	double dx = x - other.x;
+	double dy = y - other.y;
+	return sqrt(pow(dx, 2) + pow(dy, 2));
+}
+
+template<class T, class U>
+bool Point<T, U>::operator>(Point other) {
+	if (y == other.y)
+		return x > other.x;
+	return y > other.y;
+}
+
+template<class T, class U>
+void Point<T, U>::print() {
+	cout << "(" << x << "," << y << ") ";
+}
+
+template<class T, class U>
+void sort(Point<T, U>* a, int n) {
+	for (int i = 0; i < n - 1; i++)
+		for (int j = i + 1; j < n; j++)
+			if (a[i] > a[j])
+				swap(a[i], a[j]);
+}
+
+template<class T, class U>
+void twoFarthest(Point<T, U>* a, int n,double& maxDis, Point<T,U>&f1, Point<T,U>&f2) {
+	maxDis = 0;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++) {
+			double dis = a[i].calDis(a[j]);
+			//cout << dis << endl;
+			if (dis > maxDis) {
+				maxDis = dis;
+				f1 = a[i];
+				f2 = a[j];
+			}
+		}
+}
+
+
+int main() {
+	
+	int n = 4;
+	Point<double, long long> a[4] = { {5.4,1}, {-0.5,-1}, {1e5,100}, {-1,-1} };
+
+	cout << "Point Array:\n";
+	for (int i = 0; i < n; i++)
+		a[i].print();
+	cout << "\n------------------------------------------------\n";
+
+	cout << "Distance from ";
+	a[0].print();
+	cout << " to ";
+	a[1].print();
+	cout << "is: " << a[0].calDis(a[1]);
+	cout << "\n------------------------------------------------\n";
+
+	Point<double, long long> f1, f2;
+	double maxDis;
+	twoFarthest(a, n,maxDis, f1, f2);
+	cout << "Two farthest points are:\n";
+	f1.print();
+	f2.print();
+	cout << endl;
+	cout << "Their distance is: " << maxDis;
+	cout << "\n------------------------------------------------\n";
+
+	sort(a, n);
+	cout << "Point Array After Sorting:\n";
+	for (int i = 0; i < n; i++)
+		a[i].print();
+	cout << "\n------------------------------------------------\n";
+
+	return 0;
+}
